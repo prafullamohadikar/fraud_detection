c5b3cfa4493913a1bd3e4174a9e7c9e0c4004b48
prafullamohadikar <prafulla.mohadikar@gmail.com>
2023-10-29 01:26:14 +0530

    added 3 new ml model code

--- ml_model.py
+++ ml_model.py
@@ -1,3 +1,15 @@
 
 
 print("adding all ML related code here")
+
+
+def model1():
+    print("adding code for ML Model1")
+
+
+def model2():
+    print("adding code for ML Model1")
+
+
+def model3():
+    print("adding code for ML Model1")
